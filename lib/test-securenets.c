f1df1c7dc4e7a283a15f99f8bb750beca6dc0662
Thorsten Kukuk <kukuk@thkukuk.de>
2014-10-31 08:25:18 +0000

    Fix CVS

---
+++ lib/test-securenets.c
@@ -0,0 +1,29 @@
+/* Copyright (c) 2014  Thorsten Kukuk
+   Author: Thorsten Kukuk <kukuk@suse.de>
+
+   The YP Server is free software; you can redistribute it and/or
+   modify it under the terms of the GNU General Public License
+   version 2 as published by the Free Software Foundation.
+
+   The YP Server is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>. */
+
+#include "access.h"
+
+extern int debug_flag;
+
+int
+main (void)
+{
+  debug_flag = 1;
+
+  load_securenets ();
+  /* dump_securenets (); */
+
+  return 0;
+}
